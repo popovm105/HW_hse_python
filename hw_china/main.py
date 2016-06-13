b5ab06f5e4040934aeee8d6aac0b5606f01ccd21
Popov Mikhail <popovm105@yandex.ru>
2016-06-13 21:43:06 +0300

    fixed bugs, add result and data files

--- hw_china/main.py
+++ hw_china/main.py
@@ -1,16 +1,19 @@
-from bs4 import BeautifulSoup
 import string
+import re
 
 def get_new_item_to_dict(china_dict, line):
     if line.startswith('#') or line.startswith('%'):
         return
+
+    transcr = re.findall('\[.*?\]', line)[0].strip('[]')
+    sem = re.findall('/.*/', line)[0].replace(' ', '_').strip('/').replace('/',', ')
     line = line.split()
 
     if line[1] in china_dict:
-        china_dict[line[1]].append({'transcr': line[2], 'sem':line[3]})
+        china_dict[line[1]].append({'transcr': transcr, 'sem': sem})
     else:
 
-        china_dict[line[1]] = [{'transcr': line[2], 'sem':line[3]}]
+        china_dict[line[1]] = [{'transcr': transcr, 'sem': sem}]
 
 
 def check_substring(substring, china_dict):
@@ -26,14 +29,14 @@ def get_translate(line, china_dict):
 
     if line.startswith('<se>'):
         while (len(line) > 0):
-            print(line)
+            #print(line)
             substring = check_substring(line, china_dict)
             if substring:
-                print(substring)
+                #print(substring)
                 result += '\n<w>'
                 for item in china_dict[substring]:
                     result += '<ana lex="{}" transcr="{}" sem="{}"/>'.format(substring, item['transcr'],item['sem'])
-                result += '<\w>\n'
+                result += '{}</w>'.format(substring)
                 line = line.lstrip(substring)
 
             else:
@@ -44,23 +47,23 @@ def get_translate(line, china_dict):
     else:
         return line
 
+if __name__ == "__main__":
+    chinaDict = {}
+    with open('./cedict_ts.u8') as f:
+        for line in f:
+            get_new_item_to_dict(chinaDict, line)
 
-chinaDict = {}
-with open('./cedict_ts.u8') as f:
-    for line in f:
-        get_new_item_to_dict(chinaDict, line)
 
-
-with open('./result.xml', 'w') as resultFile:
-    with open('./stal.xml') as text:
-        i = 0
-        for line in text:
-            print(i)
-            translate = get_translate(line, chinaDict)
-            print(translate)
-            resultFile.write(translate)
-            i+=1
-            '''if(i==7):
-                break'''
-            resultFile.write('</body>\n</html>')
+    with open('./result.xml', 'w') as resultFile:
+        with open('./stal.xml') as text:
+            i = 0
+            for line in text:
+                #print(i)
+                translate = get_translate(line, chinaDict)
+                #print(translate)
+                resultFile.write(translate)
+                i+=1
+                #if(i==6):
+                 #   break
+            #resultFile.write('</body>\n</html>')
 
