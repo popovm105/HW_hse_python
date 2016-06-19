98742801c30595314df82c738df2e69089dbee66
Popov Mikhail <popovm105@yandex.ru>
2016-06-20 01:39:38 +0300

    add logging

--- hw_nabokov/main.py
+++ hw_nabokov/main.py
@@ -21,7 +21,7 @@ def merge_xml(folder_path):
         para['id']  = index
     
     with open(folder_path[2:-1]+'.xml','w') as f:
-        f.write(result_xml.prettify())
+        f.write(str(result_xml))
 
 
 def similar(a, b):
@@ -30,6 +30,7 @@ def similar(a, b):
 
 
 def corrector(standard_file_path, dirty_file_path, R_O_value = 0.85):
+    os.remove('log.txt')    
     with open(standard_file_path) as f:
         text = f.read()
         
@@ -39,28 +40,37 @@ def corrector(standard_file_path, dirty_file_path, R_O_value = 0.85):
         text = f.read()
         
     dirty_bs = BeautifulSoup(text,'html.parser')
-
+    start_index = 0
+    
     for se_standard in standard_bs.findAll('se'):
-
         if se_standard['lang'] in set(['en', 'uk']):
-            for se_dirty in dirty_bs.findAll('se'):
+            current_index = start_index
+            for se_dirty in dirty_bs.findAll('se'):#[start_index:]:
+                current_index += 1
                 if se_dirty['lang'] in set(['en', 'uk']):
-                    if R_O_value <similar(se_standard.text, se_dirty.text) < 1:
-                        print(se_dirty.text)
-                        print(se_standard.text)
-                        print('='*100)
+                    if 0.85 <similar(se_standard.text, se_dirty.text.strip()) < 1:
+                        with open('log.txt','a') as f:
+                            f.write(se_dirty.text.strip()+'\n')
+                            f.write(se_standard.text+'\n')
+                            f.write(str(similar(se_standard.text, se_dirty.text.strip()))+'\n')
+                            f.write('='*100 + '\n')
+                        #print(se_dirty.text.lstrip())
+                        #print(se_standard.text)
+                        #print(similar(se_standard.text, se_dirty.text.strip()))
+                        #print('='*100)
                         se_dirty.string = se_standard.string
+                        start_index = current_index
                         
-                       
+                        break
+                        
     with open(dirty_file_path,'w') as f:
         f.write(dirty_bs.prettify())
 
 
 
-if __name__ == "__main__":  
+if __name__ == "__main__":
     merge_xml('./pale_fire_Sharyimov/')
     corrector('./pale_vera.xml','./pale_fire_Sharyimov.xml')
-
-
+    
 
 
